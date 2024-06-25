7ffebc88fce8475698f25116ff9fd68a7d881a80
saad <saadd@student.iul.ac.in>
2024-06-25 22:04:17 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
